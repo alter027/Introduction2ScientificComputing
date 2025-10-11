83ca7631fa3a610dbcba0472258133e1171b2ee5
alter027 <wsjyun749@gmail.com>
2025-10-11 23:50:51 +0800

    hw4 handwrite

--- hw4/hw4_programming.py
+++ hw4/hw4_programming.py
@@ -6,13 +6,6 @@ from scipy.integrate import quad, trapezoid
 from scipy.misc import derivative
 from pynverse import inversefunc
 
-# 2 kinds of original function: 1/(1+x^2), sin(x)
-# 5 interpolation method: Vandermonde, Newton, Lagrange(kit),
-#                         Modified Lagrange, Barycentric(kit)
-Method = ['vandermonde', 'newton', 'lagrange', 'lagrange_modified', 'barycentric']
-# different sample size: 10, 30, 50, 100, 500, 1000
-SampleSize = [10, 30, 50, 100, 500, 1000]
-
 class Given:
     def __init__(self, _func):
         if _func == 'func_1':
@@ -23,32 +16,31 @@ class Given:
     def function(self):
         return self.func
 
-diff_func = lambda _x: (_x*25*(_x**2)) / (1+25*(_x**2))
-
 # Question 1: int_0^inf 1/(1+25*x^2)
 given = Given('func_1')
 result = quad(given.function(), 0, np.inf)
-print("Q1: val:", result[0], ", abs:", result[0]-np.pi/10)
+print("Q1: val:", result[0], ", err:", result[0]-np.pi/10)
+# Q1: val: 0.31415926535897887 , abs: -4.440892098500626e-16
 
 # Question 2:
-left_diff = diff_func(np.exp(-9))
-left_result = np.exp(-9)*10
+diff_func = lambda _x: (_x*25*(_x**2)) / (1+25*(_x**2))
+left_diff = diff_func(np.exp(-10))*11
+left_result = -np.exp(-10)*11
 print(left_diff, left_result)
 
 given = Given('func_2')
 _func = given.function()
-left = np.exp(-9)
+left = np.exp(-10)
 _x = np.linspace(left, 1, 10**7)
 _y = _func(_x)
-# result = quad(func, 1e-50, 1)
-print(left)
-left_diff = 0
+right_diff = 0
 for i in range(10**7):
     cur_x = _x[i]
     second_derivative = derivative(_func, cur_x, n=2, dx=1e-7)
     if (i%10**6 == 0):
         print(cur_x, second_derivative, (second_derivative*(1-left)**3)/12)
-    left_diff += abs((second_derivative*1e-21)/12)
-result = trapezoid(_y, x=_x)
-print(result, left_diff)
-print("Q2: val:", result, ", abs:", left_diff)
+    right_diff += abs((second_derivative*1e-21)/12)
+right_result = trapezoid(_y, x=_x)
+print(right_result, right_diff)
+print("Q2: val:", left_result+right_result, ", err:", left_diff+right_diff)
+# Q2: val: -0.5454445634462095 , abs: 4.4109912383427195e-11
