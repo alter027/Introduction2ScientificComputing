9a33fdf3228dc347fe5e5dc7b6cef4c6205a2a4a
alter027 <wsjyun749@gmail.com>
2025-10-19 22:28:14 +0800

    hw5

---
+++ hw5_week7/hw5_programming.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import scipy
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
+# 2 equation: y'=-5y, y'=y-y^2 
+# 2 numerical method: forward_euler, backward_euler
+# stability
+
+f1DE = lambda t, y: -5*y
+f1Solved = lambda t, y0: np.exp(-5*t)
+f2DE = lambda t, y: y*(1-y)
+f2Solved = lambda t, y0: np.exp(t) / (np.exp(t)+((1-y0)/y0))
+
+class Numerical:
+    def __init__(self, fDE, fSolved, start, end, y0):
+        self.fDE = fDE
+        self.fSolved = fSolved
+        self.start = start
+        self.end = end
+        self.y0 = y0
+    def method(self, method, h):
+        return getattr(self, method)(h)
+    def forward_euler(self, h):
+        steps = int((self.end-self.start+h) / h)
+        t = np.linspace(self.start, self.start + (steps-1)*h, steps)
+        u = np.zeros(steps)
+        u[0] = self.y0
+        for i in range(1, steps):
+            u[i] = u[i-1] + h*self.fDE(t[i-1], u[i-1])
+        return t, u, self.fSolved(t, self.y0)
+    def backward_euler(self, h):
+        steps = int((self.end-self.start+h) / h)
+        t = np.linspace(self.start, self.start + (steps-1)*h, steps)
+        u = np.zeros(steps)
+        u[0] = self.y0
+        for i in range(1, steps):
+            relation = lambda _u: _u - u[i-1] - h*self.fDE(t[i], _u)
+            u[i] = fsolve(relation, u[i-1])[0]
+        # fsolve: Powell hybrid method
+        # An alternative, direction calculate the function since f is linear
+        return t, u, self.fSolved(t, self.y0)
+
+# problem 1
+_func = 'diff_y=-5y'
+for step in (0.1, 0.4, 0.41):
+    for _method in ('forward_euler', 'backward_euler'):
+        numerical = Numerical(f1DE, f1Solved, 0, 10, 1)
+        _t, _u, _y = numerical.method(_method, step)
+
+        # plt
+        plt.clf()
+        plt.plot(_t, _u, '-', color='r', label=_method)
+        plt.plot(_t, _y, '-', color='g', label='exact')
+        plt.xlabel("t")
+        plt.ylabel("y")
+        plt.legend()
+        plt.title(_func+'_'+_method+'_h='+str(step))
+        plt.savefig('_result/'+_func+'_'+_method+'_'+str(int(step*100))+'.png')
+        # plt.show()
+
+# problem 2
+_func = 'diff_y=y-y^2'
+for y0 in (0.4, 0.5, 0.9):
+    plt.clf()
+    for step in (0.4, 1.2, 1.9, 2, 2.1):
+        _method = 'forward_euler'
+        numerical = Numerical(f2DE, f2Solved, 0, 100, y0)
+        _t, _u, _y = numerical.method(_method, step)
+
+        # plt
+        plt.plot(_t, _u, '-', label='h='+str(step))
+    plt.plot(_t, _y, '-', color='r', label='exact')
+    plt.xlabel("t")
+    plt.ylabel("y")
+    plt.title(_func+'_'+_method+'_y0='+str(y0)+'_h='+str(step))
+    plt.legend()
+    plt.savefig('_result/'+_func+'_'+_method+'_y0='+str(y0)+'_'+str(int(step*100))+'.png')
+    # plt.show()
---
+++ hw5_week7/hw5_programming_result.md
@@ -0,0 +1,34 @@
+### Problem 1
+<img src=_result/p1.png width="400" />
+
+#### Result
+|  | h=0.1 | h=0.4 | 0.41 |
+| ---- | ---- | ---- | ---- |
+| forward_euler | <img src="_result/diff_y=-5y_forward_euler_10.png" width="200"/> | <img src="_result/diff_y=-5y_forward_euler_40.png" width="200"/> | <img src="_result/diff_y=-5y_forward_euler_41.png" width="200"/> |
+| backward_euler | <img src="_result/diff_y=-5y_backward_euler_10.png" width="200"/> | <img src="_result/diff_y=-5y_backward_euler_40.png" width="200"/> | <img src="_result/diff_y=-5y_backward_euler_41.png" width="200"/> |
+
+#### Explanation
+From lecture [note](https://hackmd.io/@teshenglin/SCMA30009_05#The-absolute-stability), we know for the form $y'=\lambda y, where y(0)=1$
+- for forward euler method, for $h < \frac{-2Real(\lambda)}{|\lambda|}$ it is absolute stable, therefore, $h < 0.4$ in this case
+    - For $h = 0.4$, it brings $u_n = (1+\lambda h)u_0 = (-1)^n$, therefore, the value jump between 1 and -1.
+- for backward euler method, it is unconditionally stable as $\lambda = -5$ is a real number.
+
+The programming result matches the analysis.
+
+### Problem 2
+<img src=_result/p2.png width="400" />
+
+#### Result
+|  | forward_euler |
+| ---- | ---- |
+| y0=0.4 | <img src="_result/diff_y=y-y^2_forward_euler_y0=0.4_210.png" width="400"/> |
+| y0=0.5 | <img src="_result/diff_y=y-y^2_forward_euler_y0=0.5_210.png" width="400"/> |
+| y0=0.9 | <img src="_result/diff_y=y-y^2_forward_euler_y0=0.9_210.png" width="400"/> |
+
+#### Result and Observation
+For $h \in (0, 2)$, it brings qualitatively correct.
+For $h = 2$, it could convergent (y0=0.5) or divergent(y0=0.4, 0.9) in different y0 settings. Moreover, if $y0*h = 1$, which means the $y_1$ will touch the convergence value right after the first step, and it will stay on it forever.
+
+#### Explanation
+It is a nonlinear ODE, with equilibrium points at $y* = 0$ (unstable) and $y* = 1$ (stable).
+So consider $y* = 1$, and get $\lambda = f(y*) = -1$, so forward Euler requires $|1-\lambda h|<1 \implies 0 < h < 2$ for stability near the point.
